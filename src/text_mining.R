d524aecb7329b10ad14b8c0e0fb4d110a1cc3801
henriquepgomide <henriquepgomide@gmail.com>
2019-07-10 09:05:06 -0300

    comment script

--- src/text_mining.R
+++ src/text_mining.R
@@ -1,22 +1,38 @@
+# Patent analysis using text mining
+# Author:
+# Ivan Santos
+# License: MIT
+
+library(tidyverse)
 library(tm)
 
-#loading in the data
+# Load Data
 setwd("C://Users//santo//OneDrive//Documents//R//text mining")
 reviews <- read.csv("reviews.csv", stringsAsFactors=FALSE)
 
-#combining all the reviews together 
+# Combine rows
 review_text <- paste(reviews$text, collapse =" ")
 
-#setting up source and corpus
+# Set up source and corpus
 review_source <- VectorSource(review_text)
 corpus <- Corpus(review_source)
 
-#cleaning
+# Clean corpus
 corpus <- tm_map(corpus, content_transformer(tolower))
 corpus <- tm_map(corpus, removePunctuation)
 corpus <- tm_map(corpus, stripWhitespace)
 corpus <- tm_map(corpus, removeWords, stopwords("english"))
 
-#making a document-term matrix
+# Make a document-term matrix
 dtm <- DocumentTermMatrix(corpus)
 dtm
+
+# Goal 1 - Compare term frequency over time ('98-'17) 
+# Goal 2 - Cluster terms 
+# Goal 3 - Cluster terms and compare with other features
+# Goal 4 - How many times the term 'recombinant' shows up for each patent (row)
+# Goal 5 - How many patents (rows) have at least the term 'recombinant'
+
+
+
+
