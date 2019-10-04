eed53818b868f06e2cefce5366736621d3c8eee7
IvanSantos86 <52744952+IvanSantos86@users.noreply.github.com>
2019-10-04 13:57:33 -0300

    Create pat_analysis.R
    organizando passos

---
+++ src/pat_analysis.R
@@ -0,0 +1,496 @@
+# Patent analysis 
+# Ivan Santos, Ph.D.
+
+library(tidyverse)
+library(tm)
+library(lubridate)
+library(ggthemes)
+library(jtools)
+library(qdap)
+
+
+# Load Data
+#Windows
+data <- read.csv2("~/GitHub/Patent_Analysis/data/data_all.csv", 
+                stringsAsFactors = FALSE) 
+#Linux
+data <- read.csv2("~/Patent_Analysis/data/data_all.csv", 
+                  stringsAsFactors = FALSE) 
+
+# 1. Evolucao temporal dos depositos de patentes --------------------------
+
+# 1.1 Preparacao dos dados ================================================
+
+# Selecionar qual banco de dados (hospedeiro)
+data <- filter(data, host == "avian")
+
+# Extracao do ano e pais de prioridade
+data$year    <- str_extract(data$priority_number,"([\\d+]{4})")
+data$country <- str_extract(data$priority_number,"([A-z]{2})")
+
+data$year        <- as.numeric(data$year)  # Convert year as numeric
+data$country.rec <- fct_lump(data$country, # Reduce the number of factors
+                             n = 5)  
+
+# Separacao do IPC (International Patent Classification)
+a <- strsplit(data$ipc, "\n")
+a <- max(sapply(a, length))
+
+data <- separate(data, 
+                 col =  ipc,
+                 sep = "\n", 
+                 into = paste0("ipc.", 
+                               seq(1, a)))
+
+rm(a)
+
+# Combinar colunas titulo e resumos
+data$text.title.abstract <- paste(data$title, 
+                                  data$abstract,sep = " ")
+
+
+# Load data assignee names reviewed
+table_assignee <- read.csv2("~/GitHub/Patent_Analysis/data/patentes_all_assignee_or.csv ")
+table_assignee$assignee.s   <- fct_lump(table_assignee$Latest.standardized.assignees...inventors.removed, n = 10) 
+table_assignee$year.p    <- str_extract(table_assignee$Priority.dates,"([\\d+]{4})")
+
+# 1.2 Analise dos dados ==========================================================
+## Análise por hospedeiro
+
+## Fig1.1 - Patentes x ano x Pais de prioridade  ---- Ok
+
+# 1. Tabela dos cinco principais paises por ano.
+table_country <- data.frame(year = data$year, country = data$country)
+table_country$p.year <- as.character(table_country$year)
+table_country$p.year <- as.numeric(table_country$p.year)
+table_country$p.country <- fct_lump(table_country$country, n = 5)  
+
+# Plot Fig. 1.1
+data %>% 
+  group_by(host) %>%
+  count(year, country.rec) %>%
+  ggplot(aes(year, n, fill = country.rec)) +
+  geom_bar(stat = "identity") + 
+  facet_wrap( ~ host, nrow = 2) +
+  theme_apa() + 
+  theme(legend.position = "bottom") +
+  ylab("") + xlab("") +
+  scale_fill_brewer(palette = "Dark2", type = "qual")
+
+
+# Fig1.2. Patentes x IPC x ano  ----
+#Table
+table_ipc <- gather(data, str_subset(names(data), "ipc."), 
+                    key = "ipc",
+                    value = "ipc.code") %>%
+  filter(ipc.code != "") %>%
+  select(year, ipc.code)
+
+# Extrair IPC nivel classe
+table_ipc$ipc.s   <- str_extract(table_ipc$ipc.code, "[^/]+")
+table_ipc$ipc.s   <- fct_lump(table_ipc$ipc.s, n = 6) 
+table_ipc$ipc.code   <- fct_lump(table_ipc$ipc.code, n = 6) 
+
+# Remover codigos que nao sao de interesse
+table_ipc <- filter(table_ipc, ipc.s != "A61K-039")
+table_ipc <- filter(table_ipc, !grepl("A61K-039*", ipc.code))
+
+# Plot Fig. 1.2
+table_ipc %>% 
+  count(year, ipc.s) %>% 
+  ggplot(aes(year, n, fill = ipc.s, col = ipc.s)) +
+  #geom_bar(stat='identity') +
+  geom_area() +
+  theme_apa() + 
+  theme(legend.position = "bottom") +
+  ylab("") + xlab("") +
+  scale_fill_brewer(palette = "Dark2", type = "qual") +
+  scale_color_brewer(palette = "Dark2", type = "qual") 
+
+
+# Fig1.3 - Patentes x Depositantes x ano
+
+# Table
+table_count_assignees <- 
+  table_assignee %>%
+  group_by(assignee.s) %>%
+  summarise(freq = n()) %>%
+  arrange(desc(freq))
+
+# Plot 
+table_assignee %>% count(year.p, assignee.s) %>% 
+  ggplot(aes(x = year.p, y = n, fill = assignee.s)) +
+  geom_bar(stat = 'identity')
+
+
+# 2 Mineracao do texto -------------------------------------
+
+# 2.1 Preparacao dos dados ===================================================
+
+## Frequencia de termos por ano baseado no titulo+resumo ou reivindicações
+
+# Frequencia de termos por ano
+
+# Criar corpus
+createTermFrequencyDf <- function(data, variable, uniqueKeyword = TRUE,
+                                  rfidf = FALSE) {
+  
+  # Create data frame with term frequency data
+  # Arguments:
+  #   data: dataframe which must have an year column
+  #   variable: variable of interest
+  #   distinct: do you need distinct keywords for each document
+  #             or the summed keywords? Default = TRUE
+  
+  vector.of.years <- unique(data$year)
+  list.of.dataframes <- list()
+  
+  for (i in 1:length(vector.of.years)){
+    temp.data <- filter(data, year == vector.of.years[i])
+    review_source <- VectorSource(eval(parse(text = paste0("temp.data$", variable))))
+    corpus <- VCorpus(review_source)
+    
+    # Clean corpus
+    corpus <- tm_map(corpus, content_transformer(tolower))
+    corpus <- tm_map(corpus, removePunctuation)
+    corpus <- tm_map(corpus, stripWhitespace)
+    corpus <- tm_map(corpus, removeWords, stopwords("english"))
+    #corpus <- tm_map(corpus, stemDocument, language = "english")
+    #corpus <- stemCompletion(corpus.temp, corpus, type = "prevalent") # Try to stem words
+    
+    if (rfidf == TRUE) {
+      dtm <- TermDocumentMatrix(corpus, control = list(weighting = weightTfIdf))
+    }
+    
+    dtm <- TermDocumentMatrix(corpus)
+    dtm.matrix <- as.matrix(dtm)
+    
+    if (uniqueKeyword == TRUE) {
+      dtm.matrix <- apply(dtm.matrix, 2, function(x) ifelse(x > 0, 1, 0))
+    }
+    
+    term.freq <- rowSums(dtm.matrix)
+    temp.data <- data.frame(year = vector.of.years[i],
+                            word = names(term.freq),
+                            frequency = term.freq)
+    
+    temp.data <- arrange(temp.data, desc(frequency))
+    
+    list.of.dataframes[[i]] <- temp.data
+    
+  }
+  data <- do.call(rbind, list.of.dataframes)
+}
+
+# Frequency Terms for Title+Abstracts
+title_abs_unique <- createTermFrequencyDf(data, "text.title.abstract", 
+                                          uniqueKeyword = TRUE, 
+                                          rfidf = TRUE)
+
+title_abs_freq   <- createTermFrequencyDf(data, "text.title.abstract", 
+                                          uniqueKeyword = FALSE, 
+                                          rfidf = TRUE)
+
+# Frequency Terms for Claims
+claims_unique  <- createTermFrequencyDf(data, "claims", uniqueKeyword = TRUE, 
+                                        rfidf = FALSE)
+
+claims_freq    <- createTermFrequencyDf(data, "claims", uniqueKeyword = FALSE, 
+                                        rfidf = FALSE)
+
+
+# 2.2 Analise dos dados ==========================================================
+
+# Tabela de frequencia de termos somando todos os anos - Tit/abs
+chart_2.1 <- 
+  title_abs_unique %>%
+  group_by(word) %>%
+  summarise(freq = sum(frequency)) %>%
+  arrange(desc(freq))
+
+# Dicionario
+terms.of.interest <- c("attenuated", "inactivated", "recombinant", "strain")
+
+# Fig2.1: Frequencia de termos de interesse (1 por patente) x Ano
+chart_1 <- title_abs_unique %>%
+  filter(word %in% terms.of.interest) %>%
+  mutate(year = year(as.Date(year, format = "%Y")),
+         year_cut = cut(year, breaks = 2))
+
+
+# Grafico de linha
+ggplot(data = chart_1, 
+       aes(x = year, y = frequency,
+           fill = word, colour = word)) +
+  geom_line()
+
+#Grafico de coluna
+ggplot(data = chart_1, 
+       aes(x = year_cut, fill = word)) +
+  geom_bar(position = position_dodge())
+
+
+#Fig2.2: frequencia de termos de interesse (total) x Ano
+chart_2 <- title_abs_freq %>%
+  filter(word %in% terms.of.interest) %>%
+  mutate(year = year(as.Date(year, format = "%Y")),
+         year_cut = cut(year, breaks = 2))
+
+# Grafico de linha
+ggplot(data = chart_2, 
+       aes(x = year, y = frequency,
+           fill = word, colour = word)) +
+  geom_line()
+
+# Grafico de coluna
+ggplot(data = chart_2, 
+       aes(x = year_cut, fill = word)) +
+  geom_bar(position = position_dodge())
+
+
+#Tabela de frequencia de termos somando todos os anos - reivindicações
+table_tm.cl <- 
+  data.cl.1 %>%
+  group_by(word) %>%
+  summarise(freq = sum(frequency)) %>%
+  arrange(desc(freq))
+
+
+# Fig2.3: Frequencia de termos (1 por patente) por anos - reivindicações
+chart_3 <- data.cl.unique %>%
+  filter(word %in% terms.of.interest) %>%
+  mutate(year = year(as.Date(year, format = "%Y")),
+         year_cut = cut(year, breaks = 2))
+
+# Grafico de linha
+ggplot(data = chart_3, 
+       aes(x = year, y = frequency,
+           fill = word, colour = word)) +
+  geom_line()
+
+# Grafico de coluna
+ggplot(data = chart_3, 
+       aes(x = year_cut, fill = word)) +
+  geom_bar(position = position_dodge())
+
+
+# Fig2.4: Frequencia de termos total por anos - reivindicações
+chart_4 <- data.cl.1 %>%
+  filter(word %in% terms.of.interest) %>%
+  mutate(year = year(as.Date(year, format = "%Y")),
+         year_cut = cut(year, breaks = 2))
+
+# Grafico de linha
+ggplot(data = chart_4, 
+       aes(x = year, y = frequency,
+           fill = word, colour = word)) +
+  geom_line()
+
+# Grafico de coluna
+ggplot(data = chart_4, 
+       aes(x = year_cut, fill = word)) +
+  geom_bar(position = position_dodge())
+
+
+# 3. Associacao de palavras ----------------------------------------------------
+
+
+##3.1: Associacoes -------------------
+#com os termos "attenuated", "inactivated", "recombinant", "strain"
+word_ass <- data.frame(doc_id = seq(1:nrow(data)),
+                       text = data$text.title.abstract)
+
+corpus.wa <- VCorpus(DataframeSource(word_ass))
+
+clean.corpus <- function(corpus) {
+  corpus <- tm_map(corpus, content_transformer(tolower))
+  corpus <- tm_map(corpus, removeWords, stopwords("english"))
+  corpus <- tm_map(corpus, removePunctuation)
+  corpus <- tm_map(corpus, stripWhitespace)
+  corpus <- tm_map(corpus, removeNumbers)
+  return(corpus)
+}
+
+corpus.wa <- clean.corpus(corpus.wa)
+
+tdm.wa <- TermDocumentMatrix(corpus.wa,control = list(weighting = weightTfIdf))
+
+# TODO -  Explorar termos mais frequentes e de maior interesse da area para 
+# encontrar eventuais padroes e tecnologias
+
+associations.recom <- findAssocs(tdm.wa, 'recombinant', 0.2)
+associations.recom <- as.data.frame(associations.recom)
+associations.recom$terms <- row.names(associations.recom)
+associations.recom$terms <- factor(associations.recom$terms, 
+                                   levels = associations.recom$terms)
+
+ggplot(associations.recom, aes( y = terms)) +
+  geom_point(aes(x = recombinant), data = associations.recom,
+             size = 5) +
+  theme_gdocs() + 
+  geom_text(aes(x = recombinant,
+                label = recombinant),
+            colour = "darkred", hjust = -.25, size = 8) +
+  theme(text = element_text(size = 20),
+        axis.title.y = element_blank())
+
+
+
+associations.inac <- findAssocs(tdm.wa, 'inactivated', 0.25)
+associations.inac <- as.data.frame(associations.inac)
+associations.inac$terms <- row.names(associations.inac)
+associations.inac$terms <- factor(associations.inac$terms, 
+                                  levels = associations.inac$terms)
+
+
+ggplot(associations.inac, aes(y = terms)) +
+  geom_point(aes(x = inactivated), data = associations.inac,
+             size = 5) +
+  theme_gdocs() + 
+  geom_text(aes(x = inactivated,
+                label = inactivated),
+            colour = "darkred", hjust = -.25, size = 8)+
+  theme(text = element_text(size = 20),
+        axis.title.y = element_blank())
+
+### 3.2 Word Network ------------
+recombinant <- data[grep("recombinant", data$text.title.abstract,ignore.case = TRUE),]
+word_network_plot(recombinant$text.title.abstract[1:10], stopwords = "english")
+
+word_associate(word_ass$text,match.string = c('recombinant'),
+               stopwords = Top200Words,network.plot = T,
+               cloud.colors = c("gray85","darkred"))
+
+
+# 3.3 Affinity Propagation Clustering -----------------------------------------
+
+library(apcluster)
+library(cluster)
+
+# 1. Criar matrix patente vs. termo
+tdm_wa <- DocumentTermMatrix(corpus.wa, control = list(weighting = weightTfIdf))
+tdm_wa <- as.matrix(tdm_wa)
+
+# Muldimensional scaling for ploting data
+mds <- 
+  tdm_wa %>%
+  dist() %>% #dissimilarity matrix 
+  cmdscale() %>% # multidimensional scale - reduction for 2 dimensions
+  as_tibble(.name_repair = "unique")
+colnames(mds) <- c("Dim.1", "Dim.2")
+
+# Preparar dados para algoritmo de propagacao por afinidade.
+# datacluster <- dist(tdm_wa) 
+datacluster <- as.matrix(mds)
+negMat      <- negDistMat(datacluster, r = 2)
+apmodel     <- apcluster(negMat)
+
+# Passar clusteres para banco de dados original
+data$cluster <- as.factor(apcluster::labels(apmodel, type = "enum"))
+
+# Filtrar cluster
+
+frequentTermCluster <- function(data, n_cluster) {
+  
+  df_filtered <- subset(data, data$cluster == n_cluster)
+  
+  corpus_filtered <- data.frame(doc_id = seq(1:nrow(df_filtered)),
+                                text = df_filtered$text.title.abstract)
+  
+  corpus_filtered <- VCorpus(DataframeSource(corpus_filtered))
+  corpus_filtered <- clean.corpus(corpus_filtered)
+  tda_filtered <- TermDocumentMatrix(corpus_filtered, 
+                                     control = list(weighting = weightTfIdf))
+  
+  tda_filtered <- as.matrix(tda_filtered)
+  words_list <- rowSums(tda_filtered)
+  terms <- sort(words_list, decreasing = TRUE)
+  
+  return(terms[1:10])
+  
+}
+
+frequentTermCluster(data, n_cluster = i)  
+
+
+# plotar experimentalmente os clusteres ----
+# Criar variaveis para plotar abaixo
+mds$country.r <- data$country.rec
+mds$year <- data$year
+mds$cluster <- as.factor(apcluster::labels(apmodel, type = "enum"))
+
+# Plot clusters
+library(ggpubr)
+ggscatter(mds, x = "Dim.1", y = "Dim.2",
+          color = "cluster",
+          repel = TRUE) +
+  facet_wrap(~ year) 
+
+
+# 4 Analise de valor das patentes ----------------------------------------------
+
+# 4.1 Create attributes for patent valuation -----------------------------------
+# 1. Select the first priority number
+data$first.priority.number <- gsub("\n(.*)", "", data$priority_number)
+
+# 2. Count inventors
+data$inventors.fr <- str_count(data$inventors, pattern = "\n") + 1
+
+# Count independent claims
+data$indep.claim.fr <-  str_count(data$indep_claim, pattern = "\n") 
+
+# Count frequency of patents by country
+
+first <- str_extract(data$family_publication, "[A-Z]{2}")
+temp <- str_extract_all(data$family_publication, "\n[A-Z]{2}")
+temp <- lapply(temp, function(x) gsub("\n", "", x))
+temp <- lapply(temp, function(x) c(x))
+temp <- lapply(temp, function(x) unique(x))
+temp <- lapply(temp, function(x) paste0(x, collapse = "|"))
+temp <- unlist(temp)
+temp <- paste(first , temp, sep = "|")
+temp <- str_extract_all(temp, "[A-Z]{2}")
+temp <- lapply(temp, function(x) unique(x))
+
+
+# Criar variavel para contagem de paises unicos por patente
+n <- lapply(temp, function(x) length(x))
+data$n.unique.contries <- unlist(n) 
+
+# Criar tabela com numero de patentes por pais
+temp <- lapply(temp, function(x) paste0(x, collapse = "|"))
+data$countries.list <- unlist(temp)
+a <- strsplit(data$countries.list, "\\|")
+a <- max(sapply(a, length))
+
+data <- separate(data, 
+                 col =  countries.list,
+                 sep = "\\|", 
+                 into = paste0("country.", 
+                               seq(1, a)))
+
+rm(a, temp, n)
+
+# 4.2 Analise dos dados ==========================================================
+
+#Patentes com maior número de titulares
+
+#Patentes com maior número de inventores
+
+#Patentes com maior número de reivindicações independentes
+
+#FamPats com maoir número de membros
+
+
+
+# Patentes por país de depósito
+table_country <- 
+  gather(data, str_subset(names(data), "country\\.[1-99]"),
+         key = "country",
+         value = "country.code") %>%
+  filter(country.code != "") %>%
+  select(country.code) %>%
+  group_by(country.code) %>%
+  count(country.code) %>%
+  arrange(desc(n)) %>%
+  filter(country.code != "WO")
